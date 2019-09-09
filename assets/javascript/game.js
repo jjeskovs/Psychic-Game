411a25a53f4cad7cffa452eba9a53e507ac251a2
Jegors Jeskovs <jegorsj@gmail.com>
2019-09-09 10:35:44 -0600

    Final working copy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,52 @@ var wins = 0
 var losses = 0
 var tries = 9
 
-
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById("tries").innerHTML = "You have " + tries + " tries left";
+    
 // ask TA how to add the upper keys as well
 var gameChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var winsText = document.getElementById("wins")
-var lossesText = document.getElementById("losses")
-var triesText = document.getElementById("tries")
+var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)]
+console.log(computerGuess)
+
+function resetGame() {
+    computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    console.log(computerGuess)
+    document.getElementById("lettersGuessed").innerHTML = "";
+    tries = 9
+    document.getElementById("tries").innerHTML = "You have " + tries + " tries left";
+} 
 
 document.onkeyup = function (event) {
-    var userGuess = event.key
-    var computerGuess = gameChoices[Math.floor(Math.random() * gameChoices.length)]
+    var userGuess = (event.key).toLowerCase()
+    
+    if (gameChoices.indexOf(userGuess) !== -1){
 
-if (userGuess === computerGuess) {
-    wins++
-} else {losses++
-        tries--  // is this correct?
+        
+        document.getElementById("lettersGuessed").innerHTML += userGuess + ", ";
+        
+        
+        
+        if (userGuess === computerGuess) {
+            wins++
+            document.getElementById('wins').innerHTML = "Wins: " + wins;
+            resetGame()
+        }else { 
+            tries--  
+            document.getElementById("tries").innerHTML = "You have " + tries + " tries left";
+        }
+        if (tries === 0) {
+            
+            alert("GAME OVER");
+            losses++
+            document.getElementById('losses').innerHTML = "Losses: " + losses;
+            resetGame()
+        }
+        
+        
+        
+    }
+    
 }
-if (tries === 0) {
-    alert("GAME OVER");
-    // document.location.reload();
-
-    winsText.textContent = "Wins: " + wins;
-    lossesText.textContent = "Losses: " + losses;
-    tiesText.textContent = "Ties: " + ties;
-
-}
-}
-
